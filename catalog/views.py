2230feb61e5c5da4f725ead8de94b40d0c258b1c
Heattehnik <gadjka@mail.ru>
2023-06-14 17:01:10 +0500

    add catalog app

---
+++ catalog/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name}\n{email}\n{message}')
+
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        print(f'{name}\n{email}\n{message}')
+
+    return render(request, 'catalog/contacts.html')
+
