5509135591f24afa960ee3f76ca4ee7e7fb40119
roman <gadjka@mail.ru>
2023-06-20 15:38:44 +0500

    refactor

--- catalog/models.py
+++ catalog/models.py
@@ -20,7 +20,7 @@ class Product(models.Model):
     name = models.CharField(max_length=50, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     preview = models.ImageField(upload_to='previews/', verbose_name='Превью', **NULLABLE)
-    category = models.ForeignKey(Category.name, on_delete=models.CASCADE, verbose_name='Категория')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создано')
     updated_at = models.DateTimeField(auto_now_add=True, verbose_name='Изменено')
